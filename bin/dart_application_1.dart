a0730317d21975abf63738158efe6ffd011b6b00
Sadid <sadid.jones@gmail.com>
2022-12-16 15:34:55 +0600

    @16/12/2022

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,20 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  final person = Person();
+  print(person.age);
+  print(person.des);
+}
+
+class Person {
+  late String des = heavyCalculation();
+  final int age;
+  Person({this.age = 18}) {
+    print("Constructor is called");
+  }
+  String heavyCalculation() {
+    print("Heavy Calculation Function is called");
+    return "Foo Bar";
+  }
+}
